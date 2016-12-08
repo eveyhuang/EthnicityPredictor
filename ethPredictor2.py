7fe9106470a6b843986848bdbb8a19035cb96f0e
Evey Huang <jiaxinh.evey@gmail.com>
2016-12-08 11:12:42 -0800

    using scikitlearn to write. vectorize features. random forest taking too long.

--- ethPredictor2.py
+++ ethPredictor2.py
@@ -10,6 +10,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
 from sklearn import metrics
+from sklearn.feature_extraction import DictVectorizer
 import pickle
 
 class ethPredictor2():
@@ -46,55 +47,64 @@ class ethPredictor2():
         }
 
     def trainAndTest(self,trainingPercent=0.80):
-        featureset = pd.DataFrame(self.getFeatures(), columns=['Features','Ethnicity'])
+        featureset = self.getFeatures()
 
         print ("Created a set of features.")
-        print(featureset.head(10))
         # random.shuffle(featureset)
         # name_count = len(featureset)
         # cut_point=int(name_count*trainingPercent)
         # self.train_set = featureset[:cut_point]
         # self.test_set  = featureset[cut_point:]
         # print ("Created training set and testing set.")
-        x_train, x_test, y_train, y_test = train_test_split(featureset.Features, featureset.Ethnicity, random_state=1)
-        print(x_train.shape)
-        print(x_test.shape)
+        self.vec = DictVectorizer()
+        X = self.vec.fit_transform([item[0] for item in featureset]).toarray()
+        Y = [item[1] for item in featureset]
+
+        print('Vectorized features.')
+        x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, train_size=0.80)
+
+        print('Split training set and testing set 0.80 and 0.20.')
         self.train(x_train,y_train)
-        return self.test(x_test)
-
-
-    # def classify(self,name):
-    #
-    #
+        return self.test(x_test, y_test)
 
     def train(self,x_train, y_train):
         self.clf_NB = MultinomialNB()
-        self.clf_RF = RandomForestClassifier(n_estimators=50, min_samples_split=6)
+        #self.clf_RF = RandomForestClassifier(n_estimators=50, min_samples_split=6)
 
         self.clf_NB.fit(x_train, y_train)
-        self.clf_RF.fit(x_train, y_train)
+        #self.clf_RF.fit(x_train, y_train)
         save_classifier = open("naiveBayes.pickle","wb")
         pickle.dump(self.clf_NB, save_classifier)
         save_classifier.close()
         print("Saved Naive Bayes classifier as naiveBayes.pickle")
 
-        save_classifier2 = open("randomForest.pickle","wb")
-        pickle.dump(self.clf_RF, save_classifier2)
-        save_classifier2.close()
-        print("Saved Random Forest classifier as randomForest.pickle")
+        # save_classifier2 = open("randomForest.pickle","wb")
+        # pickle.dump(self.clf_RF, save_classifier2)
+        # save_classifier2.close()
+        # print("Saved Random Forest classifier as randomForest.pickle")
 
 
-    # def getMostInformativeFeatures(self,n=5):
+    def getMostInformativeFeatures(self,clf,vectorizer):
+
+        print("Prints features with the highest coefficient values:")
+        feature_names = vectorizer.get_feature_names()
+        top10 = np.argsort(clf.coef_)[-10:]
+        print("%s: %s" % ("Japanese", " ".join(feature_names[j] for j in top10)))
 
 
-    def test(self,test_set):
-        NB_preds = self.clf_NB.predict(test_set)
-        RF_preds = self.clf_RF.predict(test_set)
+    def test(self,x_test, y_test):
+        NB_preds = self.clf_NB.predict(x_test)
+        #RF_preds = self.clf_RF.predict(x_test)
 
-        print("Naive Bayes accuracy and confusion matrix: ")
-        print(metrics.accuracy_score(test_set, NB_preds))
+        print("Accuracy Score: ")
+        print(metrics.accuracy_score(y_test, NB_preds))
+        print("Confusion Matrix:")
         print(metrics.confusion_matrix(y_test, NB_preds))
+        print("Auc Score:")
+        probs = nb.predict_proba(x_test)[:, 1]
+        print metrics.roc_auc_score(y_test, probs)
+        print(self.getMostInformativeFeatures(self.clf_NB, self.vec ))
 
-        print("Random Forest accuracy and confusion matrix: ")
-        print(metrics.accuracy_score(test_set, RF_preds))
-        print(metrics.confusion_matrix(y_test, RF_preds))
+        # print("Random Forest accuracy and confusion matrix: ")
+        # print(metrics.accuracy_score(test_set, RF_preds))
+        # print(metrics.confusion_matrix(y_test, RF_preds))
