289fb4a0ba4cc1fa7d2a12357a11c3ced6f5d4a1
Evey Huang <jiaxinh.evey@gmail.com>
2017-02-09 09:32:31 -0800

    balanced subset

--- ethPredictor2.py
+++ ethPredictor2.py
@@ -27,11 +27,12 @@ class ethPredictor2():
         self.df = pd.read_csv(fileName, encoding='latin-1')
         self.colName = colName
         self.ethnicityCol = ethnicityCol
+        self.ratio = self.df.groupby(ethnicityCol).count()
+        print(self.ratio)
 
     def getFeatures(self):
 
         featureset = list()
-
         for Name, Japanese in zip(self.df[self.colName], self.df[self.ethnicityCol]):
             features = self.nameFeatures(Name)
             featureset.append((features, Japanese))
@@ -50,17 +51,13 @@ class ethPredictor2():
         featureset = self.getFeatures()
 
         print ("Created a set of features.")
-        # random.shuffle(featureset)
-        # name_count = len(featureset)
-        # cut_point=int(name_count*trainingPercent)
-        # self.train_set = featureset[:cut_point]
-        # self.test_set  = featureset[cut_point:]
-        # print ("Created training set and testing set.")
+
         self.vec = DictVectorizer()
         X = self.vec.fit_transform([item[0] for item in featureset]).toarray()
         Y = [item[1] for item in featureset]
-
+        self.sample_weight = np.array([50 if i[1]==1 else 1 for i in featureset])
         print('Vectorized features.')
+        # print("length of x: " ,len(X) , "; length of y: " , len(Y))
         x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, train_size=0.80)
 
         print('Split training set and testing set 0.80 and 0.20.')
@@ -69,42 +66,47 @@ class ethPredictor2():
 
     def train(self,x_train, y_train):
         self.clf_NB = MultinomialNB()
-        #self.clf_RF = RandomForestClassifier(n_estimators=50, min_samples_split=6)
+        self.clf_RF = RandomForestClassifier(class_weight="balanced_subsample")
 
+        
         self.clf_NB.fit(x_train, y_train)
-        #self.clf_RF.fit(x_train, y_train)
-        save_classifier = open("naiveBayes.pickle","wb")
-        pickle.dump(self.clf_NB, save_classifier)
-        save_classifier.close()
-        print("Saved Naive Bayes classifier as naiveBayes.pickle")
+        self.clf_RF.fit(x_train, y_train)
+        # save_classifier = open("naiveBayes.pickle","wb")
+        # pickle.dump(self.clf_NB, save_classifier)
+        # save_classifier.close()
+        # print("Saved Naive Bayes classifier as naiveBayes.pickle")
 
-        # save_classifier2 = open("randomForest.pickle","wb")
-        # pickle.dump(self.clf_RF, save_classifier2)
-        # save_classifier2.close()
-        # print("Saved Random Forest classifier as randomForest.pickle")
-
-
-    def getMostInformativeFeatures(self,clf,vectorizer):
-
-        print("Prints features with the highest coefficient values:")
-        feature_names = vectorizer.get_feature_names()
-        top10 = np.argsort(clf.coef_)[-10:]
-        print("%s: %s" % ("Japanese", " ".join(feature_names[j] for j in top10)))
+        save_classifier2 = open("randomForest.pickle","wb")
+        pickle.dump(self.clf_RF, save_classifier2)
+        save_classifier2.close()
+        print("Saved Random Forest classifier as randomForest.pickle")
 
 
     def test(self,x_test, y_test):
         NB_preds = self.clf_NB.predict(x_test)
-        #RF_preds = self.clf_RF.predict(x_test)
+        RF_preds = self.clf_RF.predict(x_test)
 
         print("Accuracy Score: ")
         print(metrics.accuracy_score(y_test, NB_preds))
         print("Confusion Matrix:")
         print(metrics.confusion_matrix(y_test, NB_preds))
-        print("Auc Score:")
-        probs = nb.predict_proba(x_test)[:, 1]
-        print metrics.roc_auc_score(y_test, probs)
-        print(self.getMostInformativeFeatures(self.clf_NB, self.vec ))
+        # print("Classfication Report:")
+        # print(metrics.classification_report(y_test, NB_preds))
 
-        # print("Random Forest accuracy and confusion matrix: ")
-        # print(metrics.accuracy_score(test_set, RF_preds))
-        # print(metrics.confusion_matrix(y_test, RF_preds))
+        #print(self.getMostInformativeFeatures(self.clf_NB, self.vec ))
+
+        print("Random Forest accuracy and confusion matrix: ")
+        print(metrics.accuracy_score(y_test, RF_preds))
+        print(metrics.confusion_matrix(y_test, RF_preds))
+
+    
+    # def getMostInformativeFeatures(self,clf,vectorizer):
+
+    #     print("Prints features with the highest coefficient values:")
+    #     feature_names = vectorizer.get_feature_names()
+    #     top10 = np.argsort(clf.coef_)[-10:]
+    #     print(feature_names[j] for j in top10)
+
+
+
+
